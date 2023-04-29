dea1aaa26e499cbcc1adaf43ef597f5127d9bcc5
Pedro Freitas <79390915+FreitassPedro@users.noreply.github.com>
2023-04-28 22:29:42 -0300

    Questão - 2413 Busca na Internet
    https://www.beecrowd.com.br/judge/en/problems/view/2413

---
+++ Q2413.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Q2413 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int c = sc.nextInt();
+        int b = 2 * c;
+        int a = b * 2;
+        System.out.println(a);
+        sc.close();
+    }
+}
